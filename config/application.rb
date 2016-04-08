5cffe41856ad0eb07799a46ed8dba1f3b01263d5
kunok <kunokdev@gmail.com>
2016-04-08 18:19:38 +0200

    disabled assets initialization on precompile

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,7 @@ module Flightmap
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.assets.paths << Rails.root.join("vendor","assets","bower_components")
+    config.assets.initialize_on_precompile = false
 
     Flightmap::Application.configure do
       config.sass.preferred_syntax = :sass
