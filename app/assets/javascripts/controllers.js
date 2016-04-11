dd66b9dabe87edfcf72374cd9ebd80a0a2fb33b1
kunok <kunokdev@gmail.com>
2016-04-11 18:33:42 +0200

    merged controllers

--- app/assets/javascripts/controllers.js
+++ app/assets/javascripts/controllers.js
@@ -1,43 +1,9 @@
 var AppControllers = angular.module('AppControllers',[]);
 
 AppControllers.controller('MainCtrl',[
-  function(){
-}]);
-
-AppControllers.controller('SidebarCtrl', [
-  '$scope', '$timeout', '$mdSidenav', '$log', '$http',
-  function($scope, $timeout, $mdSidenav, $log, $http){
-    $scope.closeNav = function(){
-      $mdSidenav('nav').close()
-        .then(function(){
-          $log.debug("close nav is done");
-        });
-    };
-}]);
-
-
-
-AppControllers.controller('DialogCtrl', [
-  '$scope', '$mdDialog',
-  function($scope, $mdDialog) {
-  $scope.openDialog = function() {
-    $mdDialog.show(
-      $mdDialog.alert()
-        .clickOutsideToClose(true)
-        .title('Informacija o podacima')
-        .textContent('Navedeni podaci su povijesni. flightmap4.me ne preuzimaju odgovornost za njihovo korištenje prilikom donošenja odluke o rezervaciji ili kupnji aviokarte. Cijena aviokarte ovisi o trenutnoj raspoloživosti na pojedinom letu. Finalna cijena aviokarte vidljiva je na trećem koraku rezervacijskog procesa.')
-        .ariaLabel('Info dialog')
-        .ok('Zatvori')
-        .openFrom('#left')
-        .closeTo(angular.element(document.querySelector('#left')))
-    );
-  };
-}]);
-
-
-AppControllers.controller('DepartureLocationCtrl', [
   '$scope','$http',
-  function($scope,$http){
+  function($scope, $http){
+    // Departure location
     $http.get('/airports').then(function(response){
       $scope.departureLocations = response.data;
       $scope.showSuggestions();
@@ -59,43 +25,55 @@ AppControllers.controller('DepartureLocationCtrl', [
           }
         });
       });
-    }
-}]);
-
-
-AppControllers.controller('CurrencyCtrl', [
-  '$scope','$http',
-  function($scope,$http){
-    $http.get('/currencies').then(function(response){
-      $scope.availableCurrencies = response.data.currencies
-    })
-    $scope.selectedCurrency = "HRK"
-  }
-]);
-
-AppControllers.controller('PersonCountCtrl', [
-  '$scope',
-  function($scope){
+    };
+    // Person count
     limitPersonCount = function(input) {
       if (input.value < 1) input.value = 1;
       if (input.value > 100) input.value = 100;
     }
     $scope.selectedPersonCount = 1
-  }
-]);
-
-AppControllers.controller('DateCtrl', [
-  '$scope','$http',
-  function($scope, $http){
+    // Currency
+    $http.get('/currencies').then(function(response){
+      $scope.availableCurrencies = response.data.currencies
+    })
+    $scope.selectedCurrency = "HRK"
+    // Dates
     $http.get('/dates').then(function(response){
       $scope.years = response.data.years
       $scope.months = []
       for (var i = 0; i < 12; i++) {
         $scope.months.push(response.data.months[i])
       }
-    })
-  }
-]);
+    });
+}]);
+
+AppControllers.controller('SidebarCtrl', [
+  '$scope', '$timeout', '$mdSidenav', '$log', '$http',
+  function($scope, $timeout, $mdSidenav, $log, $http){
+    $scope.closeNav = function(){
+      $mdSidenav('nav').close()
+        .then(function(){
+          $log.debug("close nav is done");
+        });
+    };
+}]);
+
+AppControllers.controller('DialogCtrl', [
+  '$scope', '$mdDialog',
+  function($scope, $mdDialog) {
+  $scope.openDialog = function() {
+    $mdDialog.show(
+      $mdDialog.alert()
+        .clickOutsideToClose(true)
+        .title('Informacija o podacima')
+        .textContent('Navedeni podaci su povijesni. flightmap4.me ne preuzimaju odgovornost za njihovo korištenje prilikom donošenja odluke o rezervaciji ili kupnji aviokarte. Cijena aviokarte ovisi o trenutnoj raspoloživosti na pojedinom letu. Finalna cijena aviokarte vidljiva je na trećem koraku rezervacijskog procesa.')
+        .ariaLabel('Info dialog')
+        .ok('Zatvori')
+        .openFrom('#left')
+        .closeTo(angular.element(document.querySelector('#left')))
+    );
+  };
+}]);
 
 AppControllers.controller('DataOutput', [
   '$scope','$http',
