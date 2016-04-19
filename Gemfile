6083e5c4332d2efa34fbc1198b9f6f1f40e7330e
kunok <kunokdev@gmail.com>
2016-04-19 07:53:47 +0200

    added selection object

--- Gemfile
+++ Gemfile
@@ -55,4 +55,4 @@ group :production do
   gem 'rails_12factor'
 end
 
-ruby "2.2.1"
+#ruby "2.2.1"
--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -2,7 +2,8 @@ class CurrenciesController < ApplicationController
 
   def index
     @currencies = Ticket.distinct.pluck(:paid_currency)
-    render status:200, json: { currencies: @currencies }
+    # render status:200, json: { currencies: @currencies }
+    render status:200, json: { currencies: ["HRK"] } # temporary
   end
 
 end
