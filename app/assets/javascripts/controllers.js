f391c235c2c9acfd9e702452ef4b0857fdd20e3a
kunok <kunokdev@gmail.com>
2016-04-20 02:59:12 +0200

    fixed some small model bugs

--- app/assets/javascripts/controllers.js
+++ app/assets/javascripts/controllers.js
@@ -32,12 +32,12 @@ AppControllers.controller('MainCtrl',[
       if (input.value < 1) input.value = 1;
       if (input.value > 100) input.value = 100;
     }
-    $scope.selectedPersonCount = 1
+    $scope.selectedPersonCount = 1; // Initial person count value
     // Currency
     $http.get('/currencies').then(function(response){
       $scope.availableCurrencies = response.data.currencies
-    })
-    $scope.selectedCurrency = "HRK"
+    });
+    $scope.selectedCurrency = "HRK" // Initial currency value
     // Dates
     $http.get('/dates').then(function(response){
       $scope.years = response.data.years
@@ -71,8 +71,7 @@ AppControllers.controller('MainCtrl',[
         var departureCityDigest = departureLocationDigest[0]; // Returns City name
         var departureCountryDigest = departureLocationDigest[departureLocationDigest.length-1]; // Returns Country name
         if($scope.selectedYear == undefined) $scope.selectedYear = "*"; // sends string "*" to backend which represents "select all"
-        if($scope.selectedMonth == undefined) $scope.selectedMonth = "*";
-        // No slection object should be updated if deparute location is unset or not set properly
+        if($scope.selectedMonth == undefined || $scope.selectedMonth.no == undefined) $scope.selectedMonth = "*";
         // Selection object is a object that contains all processed selection data
         $scope.selectionObject = {
           "departure_city" : departureCityDigest,
@@ -81,11 +80,15 @@ AppControllers.controller('MainCtrl',[
           "budget_max" : $scope.budget.max,
           "person_count" : $scope.selectedPersonCount,
           "currency" : $scope.selectedCurrency,
-          "month" : $scope.selectedMonth.no,
+          "month" : $scope.selectedMonth,
           "year" : $scope.selectedYear,
           "flight_duration_min" : $scope.flightDuration.min,
           "flight_duration_max" : $scope.flightDuration.max
         };
+
+        // at this point selectionObject should be sent to backend
+        // afterwards, backend should return API
+
       } //end if
     };//end method
     //
